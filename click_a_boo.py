d825c52d96f2eb3c3602c422458e7f82c297c10e
Marcelhag <marcel.hagedorn@gmail.com>
2022-09-25 22:25:26 +0200

    Add a lot of fundtionalitis

--- click_a_boo.py
+++ click_a_boo.py
@@ -2,156 +2,83 @@
 import sys
 import subprocess
 import os
+import pandas as pd
 subprocess.check_call([sys.executable, '-m', 'pip', 'install',
 'pandas'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install',
 'openpyxl'])
 from graphics import *
-import pandas as pd
-import random
-import math
-import time
-
-class Game:
-    def __init__(self, username):
-
-        # --- In here, you can change the values as you want them ---
-        self.amount          = 10                                    # amount of circles that occur
-        self.backgroundColor = '#000000'                            # background color
-        self.circleColor     = 'red'                                # circle color
-        self.textColor       = 'white'                              # text color
-        self.minTimeBetween  = 1                                    # minimum time until next circle appears
-        self.maxTimeBetween  = 3                                    # maximum time until next circle appears
-        self.notUsedDistance = 40                                   # distance to the top, which is not used
-        self.windowWidth     = 1200                                 # width of the window
-        self.windowHeight    = math.floor(self.windowWidth/1.5)     # height of the window
-        self.minSize         = math.floor(self.windowWidth/130)     # minimum size of the circles
-        self.maxSize         = math.floor(self.windowWidth/65)      # maximum size if the circles
-        # --- --------------------------------------------------- ---
-
-        self.window = GraphWin("Click-A-Boo", width=self.windowWidth, height=self.windowHeight)
-        self.running = True
-        self.lastClick = None
-        self.startTime = time.time()
-        self.timeSinceLastClick = time.time()
-        self.timeSinceLastCorrect = time.time()
-        self.circles = []
-        self.participant = username
-        self.data = []
-        self.removeOnMistake = True
-        self.waitingTime = 0
-    
-    def createCirc(self):
-        self.timeSinceLastCorrect = time.time()
-        randTime = max(random.random() * self.maxTimeBetween, self.minTimeBetween)
-        self.waitingTime = randTime
-        time.sleep(randTime)
-        # while time.time() < self.timeSinceLastCorrect + math.floor(randTime):
-            # ...
-        size = random.randint(self.minSize, self.maxSize)
-        x = random.randint(size, self.window.width-size)
-        y = random.randint(size + self.notUsedDistance, self.window.height-size)
-        circEnt = CircleEntity(x, y, size, self)
-        self.circles.append(circEnt)
-
-    def checkMousePos(self):
-        pos = self.window.checkMouse()
-        if not pos or pos == self.lastClick:
-            return False
-        curTime = time.time()
-        self.timeSinceLastClick = curTime
-        x = pos.x
-        y = pos.y
-        deltaX = abs(self.circles[-1].x - x)
-        deltaY = abs(self.circles[-1].y - y)
-        distance = math.sqrt(pow(deltaX,2) + pow(deltaY,2))
-        if distance <= self.circles[-1].size:
-            self.circles[-1].duration = curTime - self.timeSinceLastCorrect - self.waitingTime
-            self.timeSinceLastCorrect = curTime
-            return True
-        else:
-            mistake = Mistake(distance, distance + self.circles[-1].size, curTime - self.timeSinceLastClick - self.waitingTime)
-            self.circles[-1].mistake.append(mistake)
-            return not self.removeOnMistake
-
-class Mistake:
-    def __init__(self, distance, size, duration):
-        self.distance = 0
-        self.size = size
-        self.distanceToCenter = distance + size
-        self.duration = 0
-
-class Entity:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-class CircleEntity (Entity):
-    def __init__(self, x, y, size, game):
-        super().__init__(x, y)
-        self.duration = 0
-        self.size = size
-        self.mistake = []
-        self.circ = Circle(Point(self.x, self.y), self.size)
-        self.game = game
-    
-    def draw(self):
-        self.circ.setFill(self.game.circleColor)
-        self.circ.draw(self.game.window)
-
-    def delete(self):
-        self.circ.undraw()
+from game import Game
 
 def clearConsole():
     command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+    if os.name in ('nt', 'dos'):
         command = 'cls'
     os.system(command)
+
+def generateKey():
+    day = input("Bitte gebe dein Geburtsdatum ein (DD.MM.YY): ")
+    mother = input("Bitte gebe den Vornamen deiner Mutter an: ")
+    father = input("Bitte gebe den Vornamen deines Vater an: ")
+    place = input("Bitte gebe deinen Geburtsort ein: ")
+    return (day[0:2] + mother[0] + father[-1] + place[0]).upper()
+
+def getGamesArgs():
+    gamesArgs = list()
+    path= '.\\tests'
+    print("Files and directories in a specified path:")
+    for filename in os.listdir(path):
+        gameArgs = list()
+        path_data = [i.strip().split() for i in open(path + '\\' + filename).readlines()]
+        amount = int(path_data[0][1])
+        backgroundColor = path_data[1][1]
+        circleColor = path_data[2][1]
+        textColor = path_data[3][1]
+        minTimeBetween = int(path_data[4][1])
+        maxTimeBetween = int(path_data[5][1])
+        windowWidth = int(path_data[6][1])
+        windowHeight = int(path_data[7][1])
+        notUsedDistance = int(path_data[8][1])
+        minSize = int(path_data[9][1])
+        maxSize = int(path_data[10][1])
+        removeOnMistake = True if int(path_data[11][1]) else False
+        repeat = int(path_data[12][1])
+        gameArgs.append(amount)
+        gameArgs.append(backgroundColor)
+        gameArgs.append(circleColor)
+        gameArgs.append(textColor)
+        gameArgs.append(minTimeBetween)
+        gameArgs.append(maxTimeBetween)
+        gameArgs.append(windowWidth)
+        gameArgs.append(windowHeight)
+        gameArgs.append(notUsedDistance)
+        gameArgs.append(minSize)
+        gameArgs.append(maxSize)
+        gameArgs.append(removeOnMistake)
+        gamesArgs.append((gameArgs, repeat, filename))
+        print(filename, removeOnMistake)
+    print(gamesArgs)
+    return (gamesArgs)
         
 def main():
     clearConsole()
-    username = input("Enter username: ")
-    G = Game(username)
-    G.participant = username
-    G.window.setBackground(G.backgroundColor)
-    t = Text(Point(G.window.width/2,G.window.height/2), "Hello " + G.participant + ".\nThanks for participating.\nClick anywhere to start.")
-    t.setFill(G.textColor)
-    t.draw(G.window)
-    G.window.getMouse()
-    t.undraw()
-    G.startTime = time.time()
-    G.timeSinceLastClick = G.startTime
-    G.timeSinceLastCorrect = G.startTime
-    for i in range(G.amount):
-        G.createCirc()
-        G.circles[-1].draw()
-        while not G.checkMousePos():
-            G.window.checkKey
-            print(G.window.lastKey)
-        G.circles[-1].delete()
-    G.window.close()
-    for circle in G.circles:
-        data = [circle.size, len(circle.mistake), circle.duration]
-        G.data.append(data)
-    df = pd.DataFrame(G.data, columns=['size', 'mistakes', 'duration'], dtype=float)
-    clearConsole()
-    print('Data from user: ', G.participant)
-    print('Values:\n', df, '\n')
-
-    print('Statistics:\n', df.describe())
-    with pd.ExcelWriter('results\\' + G.participant + '.xlsx') as writer:  
-        df.to_excel(writer, sheet_name='Values')
-        df.describe().to_excel(writer, sheet_name='Statistics')
-
-    # with pd.ExcelWriter('results\\Values.xlsx') as writer:  
-    #     df.to_excel(writer, sheet_name='Values')
-
-    #values = pd.read_excel('results\\Values.xlsx', sheet_name='Values')
-    #values.append(df)
-
-    #with pd.ExcelWriter('results\\Values.xlsx') as writer:  
-    #    df.to_excel(writer, sheet_name='Values')
-        
+    key = generateKey()
+    gameArgs = getGamesArgs()
+    window = GraphWin("Click-A-Boo", width=gameArgs[0][0][6], height=gameArgs[0][0][7])
+    first = True
+    for args in gameArgs:
+        data = []
+        G = Game(key, args[0])
+        for i in range(args[1]):
+            data = G.startGame(window, args[2][:-4], i, first)
+            first = False
+        df = pd.DataFrame(data, columns=['size', 'mistakes', 'duration'], dtype=float)
+        if not os.path.exists('results\\' + G.participant):
+            os.makedirs('results\\' + G.participant)
+        with pd.ExcelWriter('results\\' + G.participant + '\\' + G.participant + '_' + args[2] + '.xlsx') as writer:  
+            df.to_excel(writer, sheet_name='Values')
+            df.describe().to_excel(writer, sheet_name='Statistics')
+    window.close()
 
 
 if __name__ == "__main__":
---
+++ game.py
@@ -0,0 +1,120 @@
+import string
+from graphics import *
+import random
+import pandas as pd
+import math
+
+class Game:
+    def __init__(self, key, args: list):
+        self.amount: int                = args[0]
+        self.backgroundColor: string    = args[1]
+        self.circleColor: string        = args[2]
+        self.textColor: string          = args[3]
+        self.minTimeBetween: int        = args[4]
+        self.maxTimeBetween: int        = args[5]
+        self.windowWidth: int           = args[6]
+        self.windowHeight: int          = args[7]
+        self.notUsedDistance: int       = math.floor(self.windowWidth/args[8])
+        self.minSize: int               = math.floor(self.windowWidth/args[9])
+        self.maxSize: int               = math.floor(self.windowWidth/args[10])
+        self.removeOnMistake: bool      = args[11]
+        self.participant: string        = key
+        self.window = None
+        self.running = False
+        self.lastClick = None
+        self.startTime = None
+        self.timeSinceLastClick = None
+        self.timeSinceLastCorrect = None
+        self.circles = []
+        self.data = []
+        self.waitingTime = 0
+        self.results: pd.DataFrame = None
+
+    def startGame(self, window, testname, number, first):
+        self.data = []
+        self.window = window
+        self.running = True
+        self.window.setBackground(self.backgroundColor)
+        if first:
+            t = Text(Point(self.window.width/2,self.window.height/2), "Hello.\nThanks for participating.\nClick anywhere to start with test\n" + testname + '_' + str(number+1) + '.')
+        else:
+            t = Text(Point(self.window.width/2,self.window.height/2), "Click anywhere to start with test\n" + testname + '_' + str(number+1) + '.')
+        t.setFill(self.textColor)
+        t.draw(self.window)
+        self.window.getMouse()
+        t.undraw()
+        self.window.setBackground('#000000')
+        self.startTime = time.time()
+        self.timeSinceLastClick = self.startTime
+        self.timeSinceLastCorrect = self.startTime
+        for i in range(self.amount):
+            self.createCirc()
+            self.circles[-1].draw()
+            while not self.checkMousePos():
+                self.window.checkKey
+                print(self.window.lastKey)
+            self.circles[-1].delete()
+        for circle in self.circles:
+            data = [circle.size, len(circle.mistake), circle.duration]
+            self.data.append(data)
+        self.results = pd.DataFrame(self.data, columns=['size', 'mistakes', 'duration'], dtype=float)
+        return self.data
+
+    def createCirc(self):
+        self.timeSinceLastCorrect = time.time()
+        randTime = max(random.random() * self.maxTimeBetween, self.minTimeBetween)
+        self.waitingTime = randTime
+        time.sleep(randTime)
+        size = random.randint(self.minSize, self.maxSize)
+        x = random.randint(size, self.window.width-size)
+        y = random.randint(size + self.notUsedDistance, self.window.height-size)
+        circEnt = CircleEntity(x, y, size, self)
+        self.circles.append(circEnt)
+
+    def checkMousePos(self):
+        pos = self.window.checkMouse()
+        if not pos or pos == self.lastClick:
+            return False
+        curTime = time.time()
+        self.timeSinceLastClick = curTime
+        x = pos.x
+        y = pos.y
+        deltaX = abs(self.circles[-1].x - x)
+        deltaY = abs(self.circles[-1].y - y)
+        distance = math.sqrt(pow(deltaX,2) + pow(deltaY,2))
+        if distance <= self.circles[-1].size:
+            self.circles[-1].duration = curTime - self.timeSinceLastCorrect - self.waitingTime
+            self.timeSinceLastCorrect = curTime
+            return True
+        else:
+            mistake = Mistake(distance, distance + self.circles[-1].size, curTime - self.timeSinceLastClick - self.waitingTime)
+            self.circles[-1].mistake.append(mistake)
+            return self.removeOnMistake
+
+class Mistake:
+    def __init__(self, distance, size, duration):
+        self.distance = 0
+        self.size = size
+        self.distanceToCenter = distance + size
+        self.duration = 0
+
+class Entity:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class CircleEntity (Entity):
+    def __init__(self, x, y, size, game):
+        super().__init__(x, y)
+        self.duration = 0
+        self.size = size
+        self.mistake = []
+        self.circ = Circle(Point(self.x, self.y), self.size)
+        self.game = game
+    
+    def draw(self):
+        self.circ.setFill(self.game.circleColor)
+        self.circ.draw(self.game.window)
+
+    def delete(self):
+        self.circ.undraw()
